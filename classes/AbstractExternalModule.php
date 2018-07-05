25e5cdc9c3be52deaa88c1f136c54bd4524ba105
Mark McEver <mark@fastmail.net>
2018-07-05 16:26:34 -0500

    Removed an unnecessary require call left over from a change that was never committed

--- classes/AbstractExternalModule.php
+++ classes/AbstractExternalModule.php
@@ -1,8 +1,6 @@
 <?php
 namespace ExternalModules;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 AbstractExternalModule::init();
 
 if (class_exists('ExternalModules\AbstractExternalModule')) {
