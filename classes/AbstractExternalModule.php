anemirovsky/redcap-external-modules

commit fca4fd5fda87daddc14d4b7dc77e413954764fb5

Made it possible for developers to override the record id (when using the php api)

classes/AbstractExternalModule.php
@@ -16,7 +16,7 @@ use UIState;
 class AbstractExternalModule
 {
 	const UI_STATE_OBJECT_PREFIX = 'external-modules.';
-	const RESERVED_LOG_PARAMETER_NAMES = ['log_id', 'timestamp', 'ui_id', 'username', 'ip', 'external_module_id', 'project_id', 'record', 'message'];
+	const RESERVED_LOG_PARAMETER_NAMES = ['log_id', 'timestamp', 'ui_id', 'username', 'ip', 'external_module_id', 'project_id', 'message'];
 
 	private static $RESERVED_LOG_PARAMETER_NAMES_FLIPPED;
 
@@ -1240,7 +1240,7 @@ class AbstractExternalModule
 		$logValues['ip'] = $ip;
 		$logValues['external_module_id'] = "(select external_module_id from redcap_external_modules where directory_prefix = '{$this->PREFIX}')";
 		$logValues['project_id'] = db_real_escape_string($projectId);
-		$logValues['record'] = $recordId;
+		$logValues['record'] = "'" . db_real_escape_string($recordId) . "'";
 		$logValues['message'] = "'" . db_real_escape_string($message) . "'";
 
 		$this->query("
@@ -1254,9 +1254,12 @@ class AbstractExternalModule
 				)
 		");
 
+		$logId = db_insert_id();
 		if (!empty($parameters)) {
-			$this->insertLogParameters(db_insert_id(), $parameters);
+			$this->insertLogParameters($logId, $parameters);
 		}
+
+		return $logId;
 	}
 
 	private function insertLogParameters($logId, $parameters)

tests/AbstractExternalModuleTest.php
@@ -562,6 +562,60 @@ class AbstractExternalModuleTest extends BaseTest
 		}
 	}
 
+	function testLog_recordId()
+	{
+		$m = $this->getInstance();
+
+		$m->setRecordId(null);
+		$logId = $m->log('test');
+		$this->assertLogValues($logId, [
+			'record' => null
+		]);
+
+		$generateRecordId = function(){
+			return 'some prefix to make sure string record ids work - ' . rand();
+		};
+
+		$recordId1 = $generateRecordId();
+		$m->setRecordId($recordId1);
+		$this->logAndAssertValues('test', [
+			'record' => $recordId1
+		]);
+
+		// Make sure the detected record id can be overridden by developers
+		$this->logAndAssertValues('test', [
+			'record' => null
+		]);
+		$this->logAndAssertValues('test', [
+			'record' => $generateRecordId()
+		]);
+	}
+
+	private function logAndAssertValues($message, $params)
+	{
+		$m = $this->getInstance();
+		$logId = $m->log($message, $params);
+
+		$params['message'] = $message;
+		$this->assertLogValues($logId, $params);
+	}
+
+	// Verifies that the specified values are stored in the database under the given log id.
+	private function assertLogValues($logId, $expectedValues = [])
+	{
+		$columnNamesSql = implode(',', array_keys($expectedValues));
+		$selectSql = "select $columnNamesSql where log_id = $logId";
+
+		$m = $this->getInstance();
+		$result = $m->queryLogs($selectSql);
+		$log = db_fetch_assoc($result);
+
+		foreach($expectedValues as $name=>$expectedValue){
+			$actualValue = $log[$name];
+			$this->assertSame($expectedValue, $actualValue);
+		}
+	}
+
 	function testLog_escapedCharacters()
 	{
 		$m = $this->getInstance();

commit 2aa35a46fdb2dee1f946a2a765cdea0efa7d5bfa

Fixed a test failure caused by fca4fd5

classes/AbstractExternalModule.php
@@ -16,9 +16,10 @@ use UIState;
 class AbstractExternalModule
 {
 	const UI_STATE_OBJECT_PREFIX = 'external-modules.';
-	public static $RESERVED_LOG_PARAMETER_NAMES = ['log_id', 'timestamp', 'ui_id', 'username', 'ip', 'external_module_id', 'project_id', 'message'];
 
+	public static $RESERVED_LOG_PARAMETER_NAMES = ['log_id', 'timestamp', 'ui_id', 'username', 'ip', 'external_module_id', 'project_id', 'message'];
 	private static $RESERVED_LOG_PARAMETER_NAMES_FLIPPED;
+	private static $LOG_PARAMETERS_ON_MAIN_TABLE;
 
 	public $PREFIX;
 	public $VERSION;
@@ -1230,16 +1231,21 @@ class AbstractExternalModule
 			$projectId = 'null';
 		}
 
-		$recordId = @$parameters['record'];
-		if (empty($recordId)) {
-			$recordId = $this->getRecordIdOrTemporaryRecordId();
+		if(isset($parameters['record'])){
+			$recordId = $parameters['record'];
 
-			if (empty($recordId)) {
-				$recordId = 'null';
-			}
-			else{
-				$recordId = "'" . db_real_escape_string($recordId) . "'";
-			}
+			// Unset it so it doesn't get added to the parameters table.
+			unset($parameters['record']);
+		}
+		else{
+			$recordId = $this->getRecordIdOrTemporaryRecordId();
+		}
+
+		if (empty($recordId)) {
+			$recordId = 'null';
+		}
+		else{
+			$recordId = "'" . db_real_escape_string($recordId) . "'";
 		}
 
 		$logValues = [];
@@ -1248,7 +1254,7 @@ class AbstractExternalModule
 		$logValues['ip'] = $ip;
 		$logValues['external_module_id'] = "(select external_module_id from redcap_external_modules where directory_prefix = '{$this->PREFIX}')";
 		$logValues['project_id'] = db_real_escape_string($projectId);
-		$logValues['record'] = "'" . db_real_escape_string($recordId) . "'";
+		$logValues['record'] = $recordId;
 		$logValues['message'] = "'" . db_real_escape_string($message) . "'";
 
 		$this->query("
@@ -1346,7 +1352,7 @@ class AbstractExternalModule
 		foreach ($fields as $field) {
 			if ($field == 'username') {
 				$joinUsername = true;
-			} else if (isset(self::$RESERVED_LOG_PARAMETER_NAMES_FLIPPED[$field])) {
+			} else if (isset(self::$LOG_PARAMETERS_ON_MAIN_TABLE[$field])) {
 				// do nothing
 			} else {
 				$parameterFields[] = $field;
@@ -1392,7 +1398,7 @@ class AbstractExternalModule
 
 				if ($field === 'username') {
 					$newField = 'redcap_user_information.username';
-				} else if(isset(self::$RESERVED_LOG_PARAMETER_NAMES_FLIPPED[$field])) {
+				} else if(isset(self::$LOG_PARAMETERS_ON_MAIN_TABLE[$field])) {
 					$newField = "redcap_external_modules_log.$field";
 				} else {
 					$newField = "$field.value";
@@ -1443,5 +1449,6 @@ class AbstractExternalModule
 	public static function init()
 	{
 		self::$RESERVED_LOG_PARAMETER_NAMES_FLIPPED = array_flip(self::$RESERVED_LOG_PARAMETER_NAMES);
+		self::$LOG_PARAMETERS_ON_MAIN_TABLE = array_flip(array_merge(self::$RESERVED_LOG_PARAMETER_NAMES, ['record']));
 	}
 }

tests/AbstractExternalModuleTest.php
@@ -6,6 +6,8 @@ use \Exception;
 
 class AbstractExternalModuleTest extends BaseTest
 {
+	const UNIT_TEST_LOG_MESSAGE = 'This is a unit test log message';
+
 	protected function setUp()
 	{
 		parent::setUp();
@@ -417,7 +419,7 @@ class AbstractExternalModuleTest extends BaseTest
 		// Remove left over messages in case this test previously failed
 		$m->query('delete from redcap_external_modules_log where external_module_id = ' . $testingModuleId);
 
-		$message = 'This is a unit test log statement';
+		$message = self::UNIT_TEST_LOG_MESSAGE;
 		$paramName1 = 'testParam1';
 		$paramValue1 = rand();
 		$paramName2 = 'testParam2';
@@ -583,27 +585,16 @@ class AbstractExternalModuleTest extends BaseTest
 			return 'some prefix to make sure string record ids work - ' . rand();
 		};
 
+		$message = self::UNIT_TEST_LOG_MESSAGE;
 		$recordId1 = $generateRecordId();
 		$m->setRecordId($recordId1);
-		$this->logAndAssertValues('test', [
-			'record' => $recordId1
-		]);
+
+		$logId = $m->log($message);
+		$this->assertLogValues($logId, ['record' => $recordId1]);
 
 		// Make sure the detected record id can be overridden by developers
-		$this->logAndAssertValues('test', [
-			'record' => null
-		]);
-		$this->logAndAssertValues('test', [
-			'record' => $generateRecordId()
-		]);
-	}
-
-	private function logAndAssertValues($message, $params)
-	{
-		$m = $this->getInstance();
+		$params = ['record' => $generateRecordId()];
 		$logId = $m->log($message, $params);
-
-		$params['message'] = $message;
 		$this->assertLogValues($logId, $params);
 	}
 